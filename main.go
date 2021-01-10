677340b14e2cc5ab84a7fe8e54225bac477c1f61
Micah Resnick <resnickmicah@gmail.com>
2021-01-10 12:56:45 -0500

    Readd /refresh route

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	http.HandleFunc("/", Root)
 	http.HandleFunc("/signin", Signin)
 	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/refresh", Refresh)
 	// start the server on port 8000
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
