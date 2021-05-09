16884f1fa0ca556ebc96e6c1d5e5f2e789639ae1
Daniel Stout <ospreythirtyone@protonmail.com>
2021-05-09 11:32:14 -0400

    some style changes, date click handler

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
+        <!-- <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -22,23 +22,24 @@
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
+        <h1>Skill-Share-Club</h1>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
-    <v-main>
-      <Calendar/>
+    <v-main class="main">
+      <Calendar class="calendar-main"/>
     </v-main>
   </v-app>
 </template>
@@ -61,3 +62,9 @@ export default Vue.extend({
   }),
 });
 </script>
+
+<style scoped>
+.calendar-main {
+  height: 90vh;
+}
+</style>
